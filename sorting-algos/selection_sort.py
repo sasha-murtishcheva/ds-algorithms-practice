3a1f7dfadd2042e1e16c68538472e107233933f7
sasha-murtishcheva <a.murtischeva15@gmail.com>
2023-02-28 10:01:59 -0500

    Remove redundant condition

--- sorting-algos/selection_sort.py
+++ sorting-algos/selection_sort.py
@@ -1,14 +1,10 @@
 def find_smallest(arr):
     smallest = arr[0]
     smallest_index = 0
-    if len(arr) > 1:
-        for i in range(1, len(arr)):
-
-            if arr[i] < smallest:
-                smallest = arr[i]
-                smallest_index = i
-    else:
-        return smallest_index
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
     return smallest_index
 
 
